740615a75033ff2a8e35fd28c07b8e7d15c4dbc6
Nico Arato <nico.arato@gmail.com>
2020-02-28 23:27:42 -0300

    Componente item.

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  producto: ProductoDescripcion;
+  id: string;
+  constructor(private _route: ActivatedRoute,
+              public _producto: ProductosService) { }
 
   ngOnInit() {
+
+    this._route.params.subscribe(resp => {
+      //console.log(resp['id']);
+
+      this._producto.getProducto(resp['id'])
+          .subscribe((producto: ProductoDescripcion) => {
+            this.producto = producto;
+            this.id = resp['id'];
+            //console.log(producto);
+          });
+    });
+
   }
 
 }
